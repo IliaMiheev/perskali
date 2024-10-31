74d60987c76956bc45f50bf73eb0a1c4b7a6734d
Ilia Miheev <imiheev774@gmail.com>
2024-10-31 08:33:57 +0300

    Update tg_bot.py
    Теперь бот выводит диалоги от начала истории к концу

--- tg_bot.py
+++ tg_bot.py
@@ -23,7 +23,8 @@ def handle_document(message):
 
     # Читаем содержимое файла и отправляем текст пользователю
     with open(message.document.file_name, 'r', encoding='utf-8') as file:
-        text = json.load(file)
+        text = json.load(file)[5:11]
+        text.reverse()
         keyboard = types.InlineKeyboardMarkup()
         button = types.InlineKeyboardButton("Удалить сообщение", callback_data=message.message_id)
         keyboard.add(button)
